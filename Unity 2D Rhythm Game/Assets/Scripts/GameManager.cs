c8a9bcc043f98b8955cfee7456ed41c381a5e54c
ryoo0930 <ryoo0930@naver.com>
2020-08-11 13:26:31 +0900

    화면 이펙트 추가

--- Unity 2D Rhythm Game/Assets/Scripts/GameManager.cs
+++ Unity 2D Rhythm Game/Assets/Scripts/GameManager.cs
@@ -13,17 +13,42 @@ public class GameManager : MonoBehaviour
     }
 
     public float noteSpeed;
-
     public enum judges { NONE = 0, BAD, GOOD, PERFECT, MISS };
+
+    public GameObject[] trails;
+    private SpriteRenderer[] trailSpriteRenderers;
 
     void Start()
     {
-        
+        trailSpriteRenderers = new SpriteRenderer[trails.Length];
+        for(int i = 0; i < trails.Length; i++)
+        {
+            trailSpriteRenderers[i] = trails[i].GetComponent<SpriteRenderer>();
+        }
     }
 
     
     void Update()
     {
+        //빛나게 하기
+        if (Input.GetKey(KeyCode.D)) ShineTrail(0);
+        if (Input.GetKey(KeyCode.F)) ShineTrail(1);
+        if (Input.GetKey(KeyCode.J)) ShineTrail(2);
+        if (Input.GetKey(KeyCode.K)) ShineTrail(3);
+        //다시 어둡게 하기
+        for(int i = 0; i < trailSpriteRenderers.Length; i++)
+        {
+            Color color = trailSpriteRenderers[i].color;
+            color.a = 0.01f;
+            trailSpriteRenderers[i].color = color;
+        }
+    }
+
+    public void ShineTrail(int index)
+    {
+        Color color = trailSpriteRenderers[index].color;
+        color.a = 0.32f;
+        trailSpriteRenderers[index].color = color;
 
     }
 }
